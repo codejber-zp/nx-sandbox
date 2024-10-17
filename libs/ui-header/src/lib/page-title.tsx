84fb0f7716f99d5c00296e8f263fabd6bec12cd9
Ziemowit Przebitkowski <codejber@ziemowitprzebitkowski.pl>
2024-10-17 22:09:51 +0200

    feat(): add header library

---
+++ libs/ui-header/src/lib/page-title.tsx
@@ -0,0 +1,11 @@
+import styles from './page-title.module.css';
+
+export function PageTitle() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to PageTitle!</h1>
+    </div>
+  );
+}
+
+export default PageTitle;
---
+++ libs/ui-header/src/lib/ui-header.tsx
@@ -0,0 +1,11 @@
+import styles from './ui-header.module.css';
+
+export function UiHeader() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to UiHeader!</h1>
+    </div>
+  );
+}
+
+export default UiHeader;
